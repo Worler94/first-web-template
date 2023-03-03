181e4b8229112633f81c79e1f2c32bca1b6144ad
Luca <guglielminiluca94@gmail.com>
2023-03-03 13:19:14 +0100

    centered logo

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -36,11 +36,11 @@ function Header() {
         <div className="flex items-center justify-between h-20">
 
           {/* Site branding */}
-          <div className="shrink-0 mr-4" style={{ right: '50%', position:'absolute'}}>
+          <div className="shrink-0 mr-4" style={{ width: '95%', position:'fixed', textAlign: 'center' }}>
 
           {/* Logo */}
             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-camera"
-                 viewBox="0 0 16 16">
+                 viewBox="0 0 16 16" style={{ margin: 'auto' }}>
               <path
                   d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"/>
               <path
